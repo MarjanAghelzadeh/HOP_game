4b6275cf09a97b6160c2c33be073b45c0ddd7a7a
Marjan Aghelzadeh <aghelzadehmarjan@gmail.com>
2022-09-02 15:34:14 +0200

    hop code added

---
+++ HOP.py
@@ -0,0 +1,12 @@
+
+def game(n):
+    for i in range(n):
+        if i%3 == 0 and i%5 == 0:
+            print('Ha')
+        elif i % 3 == 0:
+            print('Hop')
+        elif i%5 == 0:
+            print('Yo')
+        else:
+            print(i)
+game(102)
